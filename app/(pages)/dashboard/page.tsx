3f5dc0e6e7768d7bd1f47d4c3088239a90e41ab5
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-07-13 03:10:08 +0300

    add: metadata and fonts

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+import React from 'react'
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: 'For DigitalH Task description'
+}
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,5 +1,10 @@
 import { Button } from '@/components/ui/button'
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: "Login",
+  description: 'For DigitalH Task description'
+}
 export default function Home () {
   return (
     <div>
