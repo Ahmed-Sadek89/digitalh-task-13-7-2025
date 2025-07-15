7ed560dece2ed7f2bfa8434fed68f2da3fecbb1b
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-07-15 04:48:25 +0300

    fix search issue

--- components/section/CustomTable/custom-table.tsx
+++ components/section/CustomTable/custom-table.tsx
@@ -6,20 +6,28 @@ import CustomTableContent from './custom-table-content'
 import { Product } from '@/type'
 import { useProductStore } from '@/store/product-store'
 import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
 
 export default function CustomTable ({ products }: { products: Product[] }) {
+  const searchParams = useSearchParams()
+  const product_param = searchParams.get('product')
   const { productStore, setProductStore } = useProductStore()
 
   useEffect(() => {
-    if (productStore.length === 0) {
+    if (product_param) {
+      const filtered = products.filter(product =>
+        product.title.toLowerCase().includes(product_param.toLowerCase())
+      )
+      setProductStore(filtered)
+    } else {
       setProductStore(products)
     }
-  }, [products])
+  }, [products, product_param])
 
   const { table } = useTableStructure({
     data: productStore.length > 0 ? productStore : products
   })
-  
+
   return (
     <div className='w-full'>
       <CustomTableHeader table={table} />
